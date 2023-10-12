ca9c0b9750850d238a8f044bcb25e6615c9e4714
Ivanruii <107073281+Ivanruii@users.noreply.github.com>
2023-10-12 07:28:04 +0000

    Tanles creation

---
+++ sql/ejer1/tienda.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS piezas (
+    cod_pieza int AUTO_INCREMENTAL,
+    nombre VARCHAR(30),
+    fecha_fabri DATE
+)
+
+CREATE TABLE IF NOT EXISTS proveedores (
+    cod_prove int AUTO_INCREMENTAL,
+    nombre VARCHAR(30),
+    ciudad VARCHAR(20) 
+)
+
+CREATE TABLE IF NOT EXISTS precios (
+    cod_precio int AUTO_INCREMENTAL,
+    cod_pieza INTEGER,
+    cod_prove INTEGER,
+    precio FLOAT
+)

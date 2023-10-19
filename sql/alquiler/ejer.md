d7a71b260afd0fb0824adcfb407f9f3c6c78052c
Iván Ruiz <ivr.644@gmail.com>
2023-10-19 13:33:44 +0200

    alquiler y tienda

---
+++ sql/alquiler/ejer.md
@@ -0,0 +1,78 @@
+# Ejercios de SQL
+Practica de tablas y consultas básicas
+
+## Ejercio 1
+
+**1. Crear las tablas:**
+
+```sql
+CREATE TABLE clientes (
+    dni INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(25),
+    ciudad VARCHAR(15),
+    observaciones VARCHAR(50),
+    fecha_carnet DATE
+);
+
+CREATE TABLE coches (
+    matricula INT AUTO_INCREMENT PRIMARY KEY,
+    marca VARCHAR(25),
+    modelo VARCHAR(25),
+    color VARCHAR(15)
+);
+
+CREATE TABLE alquiler (
+    cod_alquiler INT AUTO_INCREMENT PRIMARY KEY,
+    dni_cliente INT,
+    matricula_coche INT,
+    fecha_inicio DATE,
+    fecha_entrega DATE,
+    dias_limite INT,
+    precio_alquiler DECIMAL(10, 2),
+);
+```
+
+**2. Agregar columna `fecha_carnet` a la tabla clientes:**
+
+```sql
+    ALTER TABLE Clientes ADD fecha_carnet DATE;
+```
+
+**3. Modificar la tabla alquiler para introducir claves ajenas.**
+
+```sql
+ALTER TABLE alquiler
+ADD CONSTRAINT fk_dni_cliente
+FOREIGN KEY (dni_cliente)
+REFERENCES clientes(dni);
+
+ALTER TABLE alquiler
+ADD CONSTRAINT fk_matricula_coche
+FOREIGN KEY (matricula_coche)
+REFERENCES coches(matricula);
+```
+
+**4. Adjuntar la imagen correspondiente a las tablas junto con las relaciones en el documento de texto.**
+
+**5. Obtener las siguientes consultas:**
+
+**5.1. Todos los datos de la tabla Alquiler:**
+
+```sql
+SELECT * FROM Alquiler;
+```
+
+**5.2. Nombre y ciudad de los clientes cuyo nombre contenga una 'f' y la ciudad contenga en la segunda letra una 'm':**
+
+```sql
+SELECT nombre, ciudad FROM Clientes
+WHERE nombre LIKE '%f%' AND SUBSTRING(ciudad, 2, 1) = 'm';
+```
+
+**5.3. Marca y color de los coches cuyo precio de alquiler esté entre 200 y 500 euros (ambos inclusive), ordenados por el color de forma descendente:**
+
+```sql
+SELECT marca, color FROM Coches
+WHERE precio_alquiler BETWEEN 200.00 AND 500.00
+ORDER BY color DESC;
+```
--- sql/tienda/tienda.sql
+++ sql/tienda/tienda.sql
@@ -36,3 +36,126 @@ REFERENCES piezas (cod_pieza) ON DELETE CASCADE ON UPDATE CASCADE;
 ALTER TABLE precios
 ADD CONSTRAINT fk_prepro FOREIGN KEY (cod_prove)
 REFERENCES proveedores (cod_prove) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- Incluir 5 tuplas
+-- Tuplas para la tabla piezas
+
+INSERT INTO piezas (nombre, fecha_fabri) VALUES
+    ('Tornillo', '2023-10-19'),
+    ('Tuerca', '2023-10-18'),
+    ('Arandela', '2023-10-17'),
+    ('Resorte', '2023-10-16'),
+    ('Clavo', '2023-10-15');
+
+-- Tuplas para la tabla proveedores
+INSERT INTO proveedores (nombre, ciudad) VALUES
+    ('Proveedor A', 'Madrid'),
+    ('Proveedor B', 'Barcelona'),
+    ('Proveedor C', 'Valencia'),
+    ('Proveedor D', 'Sevilla'),
+    ('Proveedor E', 'Bilbao');
+
+-- Tuplas para la tabla precios
+INSERT INTO precios (cod_pieza, cod_prove, precio) VALUES
+    (1, 1, 0.50),
+    (2, 2, 0.75),
+    (3, 3, 1.00),
+    (4, 4, 1.25),
+    (5, 5, 1.50);
+
+
+-- 5. Obtener las siguientes consultas:
+-- • Nombres de todos los Proveedores.
+
+SELECT nombre
+FROM proveedores;
+
+-- • Todos los datos de las Piezas que hayan sido fabricadas hoy.
+
+SELECT *
+FROM piezas
+WHERE fechaFabri = #16/10/2023#;
+
+SELECT *
+FROM piezas
+WHERE fechaFabri = DATE(); /* Da la fecha del sistema */
+
+-- • Código de los proveedores que suministran piezas entre 5 y 30 euros (ambos inclusive).
+
+SELECT cod_prove
+FROM precios
+WHERE precio BETWEEN 5 AND 30;
+
+-- • Teléfono del proveedor, cuyo nombre contenga una ‘E’ y la ciudad del proveedor contenga en la segunda letra una ‘L’.
+
+SELECT telefono
+FROM proveedores
+WHERE nombre LIKE "*E*" AND ciudad LIKE "?L*";
+
+-- • Nombre de los proveedores, sin duplicados, que hayan suministrado piezas fabricadas antes del año 2010.
+
+SELECT DISTINCT proveedores.nombre
+FROM proveedores, precios, piezas
+WHERE fechaFabri < #01/01/2010#
+AND proveedores.cod_prove = precios.cod_prove
+AND piezas.cod_pieza = precios.cod_pieza;
+
+-- • Código del proveedor cuyas piezas suministradas sean posteriores al año 2010.
+
+SELECT cod_prove
+FROM piezas, precios
+WHERE fechaFabri > #01/01/2010#
+AND piezas.cod_pieza = precios.cod_pieza;
+
+-- • El precio medio al que se nos suministran las piezas.
+
+SELECT AVG(precio)
+FROM precios;
+
+-- • El código del proveedor, junto con el precio total de las piezas que ha suministrado.
+
+SELECT cod_prove, SUM(precio)
+FROM precios
+GROUP BY cod_prove;
+
+-- • Lo mismo que la anterior pero que además el precio total haya superado los 300 €.
+
+SELECT cod_prove, SUM(precio)
+FROM precios
+GROUP BY cod_prove
+HAVING SUM(precio) > 300;
+
+-- • El código del proveedor que suministra la pieza más cara. Hacerlo con subconsulta.
+
+SELECT cod_prove
+FROM precios
+WHERE precio = (
+SELECT MAX(precio)
+FROM precios
+);
+
+-- • Todos los datos de la tabla suministran, ordenados por el precio de forma descendente.
+
+SELECT *
+FROM precios
+ORDER BY precio DESC;
+
+-- • Elimina al proveedor 3, pues ha si dado de baja, como no está fijada la integridad referencial en cascada, 
+-- escribe los pasos correspondientes para que la B.D. siga coherente. Necesita de dos consultas la 12.1 y 12.2
+
+DELETE *
+FROM precios
+WHERE cod_prove = 3;
+
+DELETE *
+FROM proveedores
+WHERE cod_prove = 3;
+
+-- • La tienda ha sido traspasada a otra, ya no necesita ninguna información sobre
+-- los proveedores, pues ella tiene los suyos y con otros datos, por lo tanto,
+-- pasamos a eliminar toda la información de los proveedores. Necesita de dos
+-- consultas la 13.1 y 13.2
+
+ALTER TABLE precios
+DROP CONSTRAINT fk_prepro;
+DROP TABLE proveedores;
